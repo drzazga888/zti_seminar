7eeca423fd80e478eb29484c1ed933453417bb43
drzazga888 <drzazga888@gmail.com>
2017-05-17 03:23:05 +0200

    Create app.js

---
+++ 4-react-hello/react-src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+      <header>
+        <h1>Aplikacja "Ludzie"</h1>
+        <p>Przykład wykorzystania frameworka <strong>ReactJS</strong> do REST-owego klienta bazy "Ludzie"</p>
+      </header>
+      <main>
+        <p>Hello React!</p>
+      </main>
+    </div>
+);
+
+export default App;
